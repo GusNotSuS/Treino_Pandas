b1c56932e9a0bd750a4b1940213e4627b1b8f9df
GusNotSuS <ggustafreire@outlook.com>
2024-12-23 10:02:01 -0300

    Alterações 4
    Adição de mais funcionalidades para o treino

--- código.py
+++ código.py
@@ -17,10 +17,17 @@ print('\n\n')
 
 df_pokemon2 = pd.DataFrame(columns=df_pokemon.columns)
 while True:
-    escolha = int(input('Digite 1 para adicionar pokemon a lista, ou 0 para sair: '))
+    escolha = int(input('Digite 1 para ver informações de um pokemon\n2 para adicionar pokemon a uma lista separada\n3 para remover pokemon da lista separda\n4 para visualisar lista\n0 para sair: '))
     if escolha == 0:
         break
     if escolha == 1:
+        pokemon = str(input('digite o nome do pokemon: ')).capitalize()
+        if pokemon in df_pokemon['Name'].values:
+            # Seleciona a linha do Pokémon escolhido
+            escolha_pokemon = df_pokemon.loc[df_pokemon['Name'] == pokemon]
+            print(escolha_pokemon)
+            print('\n')
+    if escolha == 2:
     
         # Verifica se o Pokémon está no DataFrame original
         pokemon = str(input('digite o nome do pokemon: ')).capitalize()
@@ -31,7 +38,29 @@ while True:
             df_pokemon2 = pd.concat([df_pokemon2, escolha_pokemon], ignore_index=True)
             print(f"{pokemon} foi adicionado às suas escolhas!")   
             print(f"Suas escolhas até agora:{df_pokemon2}")
+            print('\n')
         else:
             print("Pokémon não encontrado. Tente novamente.")
+
+
+
+    if escolha == 3:
+        print(df_pokemon2)
+        pokemon = input('Digite o nome do Pokémon a ser removido: ').capitalize()
+        
+
+        # Localiza as linhas onde o nome é igual ao Pokémon fornecido
+        df_pokemon2 = df_pokemon2[df_pokemon2['Name'] != pokemon]
+
+        # Mostra o DataFrame atualizado
+        print(df_pokemon2)
+        print('\n')
+    if escolha == 4:
+        print(df_pokemon2)
+        print('\n')
+
+
+
 arquivo_excel = "pokemon_escolhidos.xlsx"
+print('Arquivo com lista de pokemons criado com sucesso')
 df_pokemon2.to_excel(arquivo_excel, index=False)
